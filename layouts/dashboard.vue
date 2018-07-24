8d507e3ddee29e864f08936446d8120b47cdc76a
Skyler <firemouth55@gmail.com>
2018-07-24 07:55:37 -0600

    adding icons to dashboard

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -8,20 +8,58 @@
     >
       <v-list dense>
         <img id="logo" src="~/assets/img/logo.png" alt="" class="responsive-img">
+        <!-- view orders -->
         <v-list-tile @click="test">
           <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
+            <v-icon>account_box</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
+            <v-list-tile-title>My Account</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <!-- new order -->
+        <v-list-tile @click="test">
+          <v-list-tile-action>
+            <v-icon>note_add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>New Order</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <!-- new order -->
+        <v-list-tile @click="test">
+          <v-list-tile-action>
+            <v-icon>view_list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Current Orders</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <!-- new order -->
+        <v-list-tile @click="test">
+          <v-list-tile-action>
+            <v-icon>note_add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>New Order</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <!-- new order -->
+        <v-list-tile @click="test">
+          <v-list-tile-action>
+            <v-icon>info</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Info</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <!-- new order -->
         <v-list-tile @click="test">
           <v-list-tile-action>
             <v-icon>settings</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
+            <v-list-tile-title>Account Settings</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
